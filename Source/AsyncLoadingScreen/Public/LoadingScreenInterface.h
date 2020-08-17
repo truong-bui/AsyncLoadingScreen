8949e4259ca77c7f5bdadf3b715f2e81b0b9d8c2
Truong Bui <truongbui1689@gmail.com>
2020-08-17 22:14:42 +0700

    Updated repository url

--- Source/AsyncLoadingScreen/Public/LoadingScreenInterface.h
+++ Source/AsyncLoadingScreen/Public/LoadingScreenInterface.h
@@ -1,7 +1,7 @@
 /**********************************************************************************
  *
  * Copyright (C) 2020 Truong Bui.
- * Website:	https://github.com/truongbui1689/AsyncLoadingScreen
+ * Website:	https://github.com/truong-bui/AsyncLoadingScreen
  * Licensed under the MIT License. See 'LICENSE' file for full license information.
  * 
  **********************************************************************************/
