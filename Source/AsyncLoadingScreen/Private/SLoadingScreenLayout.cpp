2e1d1d1e445d5d401d023125a3e3cb43aa72b1b6
Truong Bui <truongbui1689@gmail.com>
2024-01-23 14:28:32 +0700

    Fix undeclared identifier errors when packaging plugin

--- Source/AsyncLoadingScreen/Private/SLoadingScreenLayout.cpp
+++ Source/AsyncLoadingScreen/Private/SLoadingScreenLayout.cpp
@@ -8,6 +8,8 @@
 
 #include "SLoadingScreenLayout.h"
 #include "Engine/UserInterfaceSettings.h"
+#include "Engine/Engine.h"
+#include "Engine/GameViewportClient.h"
 
 float SLoadingScreenLayout::PointSizeToSlateUnits(float PointSize)
 {
@@ -19,11 +21,27 @@ float SLoadingScreenLayout::PointSizeToSlateUnits(float PointSize)
 
 float SLoadingScreenLayout::GetDPIScale() const
 {
-	FVector2D ViewportSize;
-	GEngine->GameViewport->GetViewportSize(ViewportSize);	
-	int32 X = FGenericPlatformMath::FloorToInt(ViewportSize.X);
-	int32 Y = FGenericPlatformMath::FloorToInt(ViewportSize.Y);
-	const FIntPoint Size(X, Y);
+	FIntPoint Size;
+	if (GEngine && GEngine->GameViewport)
+	{
+		FVector2D ViewportSize;
+		GEngine->GameViewport->GetViewportSize(ViewportSize);
+		int32 X = FGenericPlatformMath::FloorToInt(ViewportSize.X);
+		int32 Y = FGenericPlatformMath::FloorToInt(ViewportSize.Y);
+		Size = FIntPoint(X, Y);	
+	}
+	else 
+	{
+		const FVector2D DrawSize = GetTickSpaceGeometry().ToPaintGeometry().GetLocalSize();
+		if (DrawSize.Equals(FVector2D::ZeroVector))
+		{
+			return 1.0f;
+		}		
+		int32 X = FGenericPlatformMath::FloorToInt(DrawSize.X);
+		int32 Y = FGenericPlatformMath::FloorToInt(DrawSize.Y);
+		Size = FIntPoint(X, Y);		
+	}
+
 	return FMath::Clamp(GetDefault<UUserInterfaceSettings>()->GetDPIScaleBasedOnSize(Size), 0.1f, 1.0f);
 }
 
