3d524c30b56250e4cf6a06c761cfc87a4f8b23c7
truong-bui <truongbui1689@gmail.com>
2026-01-09 17:18:51 +0700

    Add setting changing background images during loading screen

--- Source/AsyncLoadingScreen/Private/SBackgroundWidget.cpp
+++ Source/AsyncLoadingScreen/Private/SBackgroundWidget.cpp
@@ -17,14 +17,17 @@
 
 void SBackgroundWidget::Construct(const FArguments& InArgs, const FBackgroundSettings& Settings)
 {
+	Images = Settings.Images;
+	Interval = Settings.UpdateInterval;
+
 	// If there's an image defined
-	if (Settings.Images.Num() > 0)
+	if (Images.Num() > 0)
 	{
-		int32 ImageIndex = FMath::RandRange(0, Settings.Images.Num() - 1);
+		int32 ImageIndex = FMath::RandRange(0, Images.Num() - 1);
 
 		if (Settings.bSetDisplayBackgroundManually == true)
 		{
-			if (Settings.Images.IsValidIndex(UAsyncLoadingScreenLibrary::GetDisplayBackgroundIndex()))
+			if (Images.IsValidIndex(UAsyncLoadingScreenLibrary::GetDisplayBackgroundIndex()))
 			{
 				ImageIndex = UAsyncLoadingScreenLibrary::GetDisplayBackgroundIndex();
 			}
@@ -32,7 +35,7 @@ void SBackgroundWidget::Construct(const FArguments& InArgs, const FBackgroundSet
 		
 		// Load background from settings
 		UTexture2D* LoadingImage = nullptr;
-		const FSoftObjectPath& ImageAsset = Settings.Images[ImageIndex];
+		const FSoftObjectPath& ImageAsset = Images[ImageIndex];
 		UObject* ImageObject = ImageAsset.TryLoad();
 		LoadingImage = Cast<UTexture2D>(ImageObject);	
 		
@@ -51,11 +54,41 @@ void SBackgroundWidget::Construct(const FArguments& InArgs, const FBackgroundSet
 					SNew(SScaleBox)
 					.Stretch(Settings.ImageStretch)
 					[
-						SNew(SImage)						
+						BackgroundWidget = SNew(SImage)
 						.Image(ImageBrush.IsValid() ? ImageBrush->GetSlateBrush() : nullptr)						
 					]
 				]
 			];			
 		}
 	}
+}
+
+int32 SBackgroundWidget::OnPaint(const FPaintArgs& Args, const FGeometry& AllottedGeometry, const FSlateRect& MyCullingRect, FSlateWindowElementList& OutDrawElements, int32 LayerId, const FWidgetStyle& InWidgetStyle, bool bParentEnabled) const
+{
+	// Update the images if Interval > 0 and Images has more than one element
+	if (Interval > 0.0f && Images.Num() > 1)
+	{
+		TotalDeltaTime += Args.GetDeltaTime();
+
+		if (TotalDeltaTime >= Interval)
+		{
+			int32 ImageIndex = FMath::RandRange(0, Images.Num() - 1);
+
+			// Load background from settings
+			UTexture2D* LoadingImage = nullptr;
+			const FSoftObjectPath& ImageAsset = Images[ImageIndex];
+			UObject* ImageObject = ImageAsset.TryLoad();
+			LoadingImage = Cast<UTexture2D>(ImageObject);
+			
+			if (LoadingImage)
+			{
+				ImageBrush = FDeferredCleanupSlateBrush::CreateBrush(LoadingImage);
+				StaticCastSharedRef<SImage>(BackgroundWidget)->SetImage(ImageBrush.IsValid() ? ImageBrush->GetSlateBrush() : nullptr);				
+			}
+
+			TotalDeltaTime = 0.0f;
+		}
+	}
+
+	return SCompoundWidget::OnPaint(Args, AllottedGeometry, MyCullingRect, OutDrawElements, LayerId, InWidgetStyle, bParentEnabled);
 }
